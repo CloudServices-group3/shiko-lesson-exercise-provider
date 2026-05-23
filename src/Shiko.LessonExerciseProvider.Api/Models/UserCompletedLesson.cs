78aec1bc64e2ca7bcb85e9666ce92b56da73bcc2
Mattias <dalebjer88@gmail.com>
2026-05-23 08:42:16 +0200

    Add lesson exercise persistence models

---
+++ src/Shiko.LessonExerciseProvider.Api/Models/UserCompletedLesson.cs
@@ -0,0 +1,14 @@
+﻿namespace Shiko.LessonExerciseProvider.Api.Models;
+
+public class UserCompletedLesson
+{
+    public Guid UserCourseProgressId { get; set; }
+
+    public Guid LessonExerciseId { get; set; }
+
+    public DateTime CompletedAtUtc { get; set; }
+
+    public UserCourseProgress UserCourseProgress { get; set; } = null!;
+
+    public LessonExercise LessonExercise { get; set; } = null!;
+}
---
+++ src/Shiko.LessonExerciseProvider.Api/Models/UserCourseProgress.cs
@@ -0,0 +1,12 @@
+﻿namespace Shiko.LessonExerciseProvider.Api.Models;
+
+public class UserCourseProgress
+{
+    public Guid Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public Guid CourseId { get; set; }
+
+    public ICollection<UserCompletedLesson> CompletedLessons { get; set; } = [];
+}
