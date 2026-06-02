05e8aa4181917f98771c81d63c88e3b1fd1d91b6
Mattias <dalebjer88@gmail.com>
2026-06-02 22:43:49 +0200

    adds frontend cors

--- src/Shiko.LessonExerciseProvider.Api/Program.cs
+++ src/Shiko.LessonExerciseProvider.Api/Program.cs
@@ -80,7 +80,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:3000",
-                "https://localhost:3000"
+                "https://localhost:3000",
+                "https://shiko-frontend-silk.vercel.app "
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
